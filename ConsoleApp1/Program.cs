206449f2b59f998be802af962b9ee82802d53bb4
DavidLejeune <Lejeune.David@outlook.Com>
2019-01-12 21:24:03 +0100

    arrays

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -247,6 +247,48 @@ namespace ConsoleApp1
             Console.WriteLine("\nintVal Type : {0}",
                 intVal.GetType());
 
+            // ----- ARRAYS -----
+            // Arrays are just boxes inside of a bigger box
+            // that can contain many values of the same
+            // data type
+            // Each item is assigned a key starting at 0
+            // and incrementing up from there
+
+            // Define an array which holds 3 values
+            // Arrays have fixed sizes
+            int[] favNums = new int[3];
+
+            // Add a value to the array
+            favNums[0] = 23;
+
+            // Retrieve a value 
+            Console.WriteLine("favNum 0 : {0}", favNums[0]);
+
+            // Create and fill array
+            string[] customers = { "Bob", "Sally", "Sue" };
+
+            // You can use var to create arrays, but the
+            // values must be of the same type
+            var employees = new[] { "Mike", "Paul", "Rick" };
+
+            // Create an array of base objects which is the 
+            // base type of all other types
+            object[] randomArray = { "Paul", 45, 1.234 };
+
+            // GetType knows its true type
+            Console.WriteLine("randomArray 0 : {0}",
+                randomArray[0].GetType());
+
+            // Get number of items in array
+            Console.WriteLine("Array Size : {0}",
+                randomArray.Length);
+
+            // Use for loop to cycle through the array
+            for (int i = 0; i < randomArray.Length; i++)
+            {
+                Console.WriteLine("Array {0} : Value : {1}",
+                i, randomArray[i]);
+            }
 
 
 
