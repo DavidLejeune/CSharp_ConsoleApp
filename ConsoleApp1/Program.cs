bde1693cc1567ba183196328e65354930885f589
DavidLejeune <Lejeune.David@outlook.Com>
2019-01-12 01:40:29 +0100

    Put actions into functions

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,18 +9,43 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello first C# console app");
 
-            Console.WriteLine("These are the args : ");
+
+
+
+            SayHello();
+            PrintArgs(args);
+            PrintArgsAsJoinedStringList(args);
+
+            Console.ReadLine();
+            
+        }
+
+        private static void PrintArgs(string[] args)
+        {
+            Console.WriteLine("\nPrintArgs >> ");
             for (int i = 0; i < args.Length; i++)
             {
-                Console.WriteLine("Arg {0} : {1} ", i , args[i]);
+                Console.WriteLine("Arg {0} : {1} ", i, args[i]);
             }
 
+        }
+
+
+        private static void PrintArgsAsJoinedStringList(string[] args)
+        {
+            Console.WriteLine("\nPrintArgsAsJoinedStringList >>");
             string[] myArgs = Environment.GetCommandLineArgs();
             Console.WriteLine(string.Join(", ", myArgs));
 
-            Console.ReadLine();
+        }
 
 
+        private static void SayHello()
+        {
+            string name = "";
+            Console.Write("What is your name : ");
+            name = Console.ReadLine();
+            Console.WriteLine("Hello {0} ", name);
         }
         
     } 
