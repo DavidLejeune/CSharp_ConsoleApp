01a792e90ecbabc747fcf92a35bf182819b58d3a
DavidLejeune <Lejeune.David@outlook.Com>
2019-01-12 01:01:34 +0100

    Hello world

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello First  World");
+            Console.WriteLine("Hello first C# console app");
 
 
             Console.ReadLine();
