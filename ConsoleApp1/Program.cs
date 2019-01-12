f47cd54183448d1f7b352192fe1982e3693a8548
DavidLejeune <Lejeune.David@outlook.Com>
2019-01-12 02:08:45 +0100

    DataTypes

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,13 +12,60 @@ namespace ConsoleApp1
 
 
 
-            SayHello();
-            PrintArgs(args);
-            PrintArgsAsJoinedStringList(args);
+            //SayHello();
+            //PrintArgs(args);
+            //PrintArgsAsJoinedStringList(args);
+            DataTypes();
+
 
             Console.ReadLine();
             
         }
+
+
+        private static void DataTypes()
+        {
+            Console.WriteLine("\nDataTypes >>");
+            bool canIVote = true;
+            Console.WriteLine("Biggest integer : {0}", int.MaxValue);
+            Console.WriteLine("Smallest integer : {0}", int.MinValue);
+            Console.WriteLine("Biggest long : {0}", long.MaxValue);
+            Console.WriteLine("Smallest long : {0}", long.MinValue);
+
+            decimal decPiVal = 3.1456231564465432165413165131651321543215M;
+            decimal decBigNum = 3.000054M;
+            Console.WriteLine("DEC : Pi + Bignum = {0}", decPiVal + decBigNum);
+            Console.WriteLine("Biggest decimal : {0}", decimal.MaxValue);
+            Console.WriteLine("Smallest decimal : {0}", decimal.MinValue);
+
+            double doublePiVal = 3.14562315644654;
+            double doubleBigNum = 3.000054;
+            Console.WriteLine("double : Pi + Bignum = {0}", doublePiVal + doubleBigNum);
+            Console.WriteLine("Biggest double : {0}", double.MaxValue.ToString("#"));
+            Console.WriteLine("Smallest double : {0}", double.MinValue.ToString("#"));
+
+
+            float floatPiVal = 3.14562354F;
+            float floatBigNum = 3.000054F;
+            Console.WriteLine("float : Pi + Bignum = {0}", floatPiVal + floatBigNum);
+            Console.WriteLine("Biggest float : {0}", float.MaxValue.ToString("#"));
+            Console.WriteLine("Smallest float : {0}", float.MinValue.ToString("#"));
+
+            bool boolFromStr = bool.Parse("true");
+            int intFromStr = int.Parse("5");
+            Console.WriteLine("boolFromStr : {0}", boolFromStr);
+
+            DateTime awesomeDate = new DateTime(1977, 6, 8);
+            Console.WriteLine("Day of week {0}", awesomeDate.DayOfWeek);
+            awesomeDate = awesomeDate.AddDays(4);
+            Console.WriteLine("New date {0}", awesomeDate.Date);
+
+            TimeSpan lunchTime = new TimeSpan(12, 30, 0);
+            lunchTime = lunchTime.Subtract(new TimeSpan(0, 25, 0));
+            Console.WriteLine("New TimeSpan : {0}", lunchTime);
+
+        }
+
 
         private static void PrintArgs(string[] args)
         {
