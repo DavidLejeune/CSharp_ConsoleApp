fa0773f29c0008411f03f4369549eb7e77de2395
DavidLejeune <Lejeune.David@outlook.Com>
2019-01-13 19:58:27 +0100

    constructors, setters, getters,  properties

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -15,76 +15,33 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
 
-            // Create a Rectangle
-            Rectangle rect1;
+            Animal cat = new Animal();
 
-            // Add values to it and run the Area method
-            rect1.length = 200;
-            rect1.width = 50;
-            Console.WriteLine("Area of rect1 : {0}",
-                rect1.Area());
+            // Call the setter
+            cat.SetName("Whiskers");
 
-            // Use a constructor to create a Rectangle
-            Rectangle rect2 = new Rectangle(100, 40);
+            // Set the property
+            cat.Sound = "Meow";
 
-            // If you assign one Rectangle to another
-            // you are setting the values and not
-            // creating a reference
-            rect2 = rect1;
-            rect1.length = 33;
+            Console.WriteLine("The cat is named {0} and says {1}",
+                cat.GetName(), cat.Sound);
 
-            Console.WriteLine("rect2.length : {0}",
-                rect2.length);
+            // Test auto generated getters and setters
+            cat.Owner = "Derek";
 
-            // ----- OBJECT ORIENTED PROGRAMMING -----
-            // A class models real world objects by
-            // defining their attributes (fields) and
-            // capabilities (methods)
-            // Then unlike with structs you can 
-            // inherit from a class and create more
-            // specific subclass types
+            Console.WriteLine("{0} owner is {1}",
+                cat.GetName(), cat.Owner);
 
-            // Add a class Project -> Add Class
+            // Get the read-only id number
+            Console.WriteLine("{0} shelter id is {1}",
+                cat.GetName(), cat.idNum);
 
-            // Create an Animal object
-            // You could also assign values like
-            // fox.name = "Red"
-            Animal fox = new Animal()
-            {
-                name = "Red",
-                sound = "Raaaw"
-            };
-
-            // Call the static method
-            Console.WriteLine("# of Animals {0}",
-                Animal.GetNumAnimals());
-            fox.MakeSound();
+            // Test static property
+            Console.WriteLine("# of Animals : {0}",
+                Animal.NumOfAnimals);
 
 
-            // You can also create static utility
-            // classes Project -> Add Class
-            Console.WriteLine("Area of Rectangle : {0}",
-                ShapeMath.GetArea("rectangle", 5, 6));
-
-            // ----- NULLABLE TYPES -----
-            // Data types by default cannot have a
-            // value of null. Often null is needed
-            // when you are working with databases
-            // and you can create a null type by 
-            // adding a ? to the definition
-            int? randNum = null;
-
-            // Check for null
-            if (randNum == null)
-            {
-                Console.WriteLine("randNum is null");
-            }
-
-            // Another check for null
-            if (!randNum.HasValue)
-            {
-                Console.WriteLine("randNum is null");
-            }
+            Console.ReadLine();
 
             // Excepts input up until a newline, but it is here to 
             // keep the console open after output
