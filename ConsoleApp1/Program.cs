e28fc46baf0736d11ce31825b8fc35af45ea8a11
DavidLejeune <Lejeune.David@outlook.Com>
2019-01-13 16:12:18 +0100

    params

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -15,40 +15,14 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
 
-            // ----- PASSING BY VALUE -----
-            double x = 5;
-            double y = 4;
+            // ----- PARAMS -----
+            // You are able to pass a variable amount
+            // of data of the same data type into a 
+            // method using params. You can also pass
+            // in an array. 
+            Console.WriteLine("1 + 2 + 3 + 5 = {0}",
+                GetSumMore(1, 2, 3, 5));
 
-            Console.WriteLine("5 + 4 = {0}",
-                GetSum(x, y));
-
-            // Even though the value for x changed in 
-            // method it remains unchanged here
-            Console.WriteLine("x = {0}",
-                x);
-
-            // ----- OUT PARAMETER -----
-            // You can pass a variable as an output 
-            // variable even without assigning a
-            // value to it
-            int solution;
-
-            // A parameter passed with out has its
-            // value assigned in the method
-            DoubleIt(15, out solution);
-
-            Console.WriteLine("15 * 2 = {0}",
-                solution);
-
-            // ----- PASS BY REFERENCE -----
-            int num1 = 10;
-            int num2 = 20;
-
-            Console.WriteLine("Before Swap num1 : {0} num2 : {1}", num1, num2);
-
-            Swap(ref num1, ref num2);
-
-            Console.WriteLine("After Swap num1 : {0} num2 : {1}", num1, num2);
 
 
 
@@ -60,42 +34,19 @@ namespace ConsoleApp1
 
         }
 
-        // ----- OUT PARAMETER -----
-        // A parameter marked with out must be assigned 
-        // a value in the method
-        static void DoubleIt(int x, out int solution)
+
+        // ----- PARAMS -----
+        // The params array must be the last parameter
+        // in the list
+        static double GetSumMore(params double[] nums)
         {
-            solution = x * 2;
+            double sum = 0;
+            foreach (int i in nums)
+            {
+                sum += i;
+            }
+            return sum;
         }
-
-        // ----- PASS BY REFERENCE -----
-        // If a variable is passed by reference changes
-        // to its value in the method effect it outside
-        // of the method
-        public static void Swap(ref int num1, ref int num2)
-        {
-            int temp = num1;
-            num1 = num2;
-            num2 = temp;
-        }
-
-        // ----- PASS BY VALUE -----
-        // By default values are passed into a method
-        // and not a reference to the variable passed
-        // Changes made to those values do not effect the
-        // variables outside of the method
-
-        // If you assign a value then it is optional to 
-        // pass
-        static double GetSum(double x = 1, double y = 1)
-        {
-            double temp = x;
-            x = y;
-            y = temp;
-            return x + y;
-        }
-
-
 
     }
 }
