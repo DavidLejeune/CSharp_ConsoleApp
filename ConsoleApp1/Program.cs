f3a47913084fc3d0869c552002bd195bf232f037
DavidLejeune <Lejeune.David@outlook.Com>
2019-01-13 16:44:47 +0100

    oop using a class

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -15,28 +15,35 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
 
-            // Create a Rectangle
-            Rectangle rect1;
+            // ----- OBJECT ORIENTED PROGRAMMING -----
+            // A class models real world objects by
+            // defining their attributes (fields) and
+            // capabilities (methods)
+            // Then unlike with structs you can 
+            // inherit from a class and create more
+            // specific subclass types
 
-            // Add values to it and run the Area method
-            rect1.length = 200;
-            rect1.width = 50;
-            Console.WriteLine("Area of rect1 : {0}",
-                rect1.Area());
+            // Add a class Project -> Add Class
 
-            // Use a constructor to create a Rectangle
-            Rectangle rect2 = new Rectangle(100, 40);
+            // Create an Animal object
+            // You could also assign values like
+            // fox.name = "Red"
+            Animal fox = new Animal()
+            {
+                name = "Red",
+                sound = "Raaaw"
+            };
 
-            // If you assign one Rectangle to another
-            // you are setting the values and not
-            // creating a reference
-            rect2 = rect1;
-            rect1.length = 33;
+            Animal dog = new Animal()
+            {
+                name = "Blue",
+                sound = "Woef"
+            };
+            // Call the static method
+            Console.WriteLine("# of Animals {0}",
+                Animal.GetNumAnimals());
 
-            Console.WriteLine("rect2.length : {0}",
-                rect2.length);
-
-
+            
 
             // Excepts input up until a newline, but it is here to 
             // keep the console open after output
@@ -45,29 +52,13 @@ namespace ConsoleApp1
 
         }
 
-        // ----- STRUCTS -----
-        // A struct is a user defined type that
-        // contain multiple fields and methods
-
-        struct Rectangle
-        {
-            public double length;
-            public double width;
-
-            // You can create a constructor method
-            // that will set the values for fields
-            public Rectangle(double l = 1, double w = 1)
-            {
-                length = l;
-                width = w;
-            }
-
-            public double Area()
-            {
-                return length * width;
-            }
-        }
+    
 
     }
+
+
+
+
+
 }
 
