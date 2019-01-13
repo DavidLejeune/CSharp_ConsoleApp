e4fdca2fb5b8d10b0415dae717140dfb2efcd820
DavidLejeune <Lejeune.David@outlook.Com>
2019-01-13 16:54:16 +0100

    static utility class

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -15,6 +15,27 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
 
+            // Create a Rectangle
+            Rectangle rect1;
+
+            // Add values to it and run the Area method
+            rect1.length = 200;
+            rect1.width = 50;
+            Console.WriteLine("Area of rect1 : {0}",
+                rect1.Area());
+
+            // Use a constructor to create a Rectangle
+            Rectangle rect2 = new Rectangle(100, 40);
+
+            // If you assign one Rectangle to another
+            // you are setting the values and not
+            // creating a reference
+            rect2 = rect1;
+            rect1.length = 33;
+
+            Console.WriteLine("rect2.length : {0}",
+                rect2.length);
+
             // ----- OBJECT ORIENTED PROGRAMMING -----
             // A class models real world objects by
             // defining their attributes (fields) and
@@ -34,16 +55,18 @@ namespace ConsoleApp1
                 sound = "Raaaw"
             };
 
-            Animal dog = new Animal()
-            {
-                name = "Blue",
-                sound = "Woef"
-            };
             // Call the static method
             Console.WriteLine("# of Animals {0}",
                 Animal.GetNumAnimals());
+            fox.MakeSound();
 
-            
+
+            // You can also create static utility
+            // classes Project -> Add Class
+            Console.WriteLine("Area of Rectangle : {0}",
+                ShapeMath.GetArea("rectangle", 5, 6));
+
+
 
             // Excepts input up until a newline, but it is here to 
             // keep the console open after output
@@ -52,7 +75,28 @@ namespace ConsoleApp1
 
         }
 
-    
+        // ----- STRUCTS -----
+        // A struct is a user defined type that
+        // contain multiple fields and methods
+
+        struct Rectangle
+        {
+            public double length;
+            public double width;
+
+            // You can create a constructor method
+            // that will set the values for fields
+            public Rectangle(double l = 1, double w = 1)
+            {
+                length = l;
+                width = w;
+            }
+
+            public double Area()
+            {
+                return length * width;
+            }
+        }
 
     }
 
