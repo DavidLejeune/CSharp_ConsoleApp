598c774f6ea969b9bf9d7ccff3439a8fd93400ec
DavidLejeune <Lejeune.David@outlook.Com>
2019-01-14 21:23:38 +0100

    getattackresult

--- ConsoleApp1/Battle.cs
+++ ConsoleApp1/Battle.cs
@@ -32,5 +32,50 @@ namespace ConsoleApp1
             }
         }
 
+        // Accept 2 Warriors
+        public static string GetAttackResult(Warrior warriorA,
+            Warrior warriorB)
+        {
+            // Calculate one Warriors attack and the others block
+            double warAAttkAmt = warriorA.Attack();
+            double warBBlkAmt = warriorB.Block();
+
+            // Subtract block from attack
+            double dmg2WarB = warAAttkAmt - warBBlkAmt;
+
+            // If there was damage subtract that from the health
+            if (dmg2WarB > 0)
+            {
+                warriorB.Health = warriorB.Health - dmg2WarB;
+            }
+            else dmg2WarB = 0;
+
+            // Print out info on who attacked who and for how
+            // much damage
+            Console.WriteLine("{0} Attacks {1} and Deals {2} Damage",
+                warriorA.Name,
+                warriorB.Name,
+                dmg2WarB);
+
+            // Provide output on the change to health
+            Console.WriteLine("{0} Has {1} Health\n",
+                warriorB.Name,
+                warriorB.Health);
+
+            // Check if the warriors health fell below
+            // zero and if so print a message and send
+            // a response that will end the loop
+            if (warriorB.Health <= 0)
+            {
+                Console.WriteLine("{0} has Died and {1} is Victorious\n",
+                    warriorB.Name,
+                    warriorA.Name);
+
+                return "Game Over";
+            }
+            else return "Fight Again";
+        }
+
+
     }
 }
