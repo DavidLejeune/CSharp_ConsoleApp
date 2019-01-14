8094a9c7fe2219b65e63ec4c37ae27aa3e16d3c9
DavidLejeune <Lejeune.David@outlook.Com>
2019-01-14 21:11:53 +0100

    warrior properties

--- ConsoleApp1/Warrior.cs
+++ ConsoleApp1/Warrior.cs
@@ -6,7 +6,11 @@ namespace ConsoleApp1
 {
     class Warrior
     {
-
+        // Define the Warriors properties
+        public string Name { get; set; } = "Warrior";
+        public double Health { get; set; } = 0;
+        public double AttkMax { get; set; } = 0;
+        public double BlockMax { get; set; } = 0;
 
     }
 }
