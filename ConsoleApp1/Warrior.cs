86fd435224b72e242a596412c0398673f14f12cb
DavidLejeune <Lejeune.David@outlook.Com>
2019-01-14 21:13:06 +0100

    warrior constructor

--- ConsoleApp1/Warrior.cs
+++ ConsoleApp1/Warrior.cs
@@ -12,5 +12,19 @@ namespace ConsoleApp1
         public double AttkMax { get; set; } = 0;
         public double BlockMax { get; set; } = 0;
 
+        // Constructor initializes the warrior
+        public Warrior(string name = "Warrior",
+            double health = 0,
+            double attkMax = 0,
+            double blockMax = 0)
+        {
+            Name = name;
+            Health = health;
+            AttkMax = attkMax;
+            BlockMax = blockMax;
+        }
+
+
+
     }
 }
