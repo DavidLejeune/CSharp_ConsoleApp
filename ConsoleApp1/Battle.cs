da1bd76b3119d62b6a2be0cbfb89d15da9b2858a
DavidLejeune <Lejeune.David@outlook.Com>
2019-01-14 21:17:47 +0100

    battle.start fight

--- ConsoleApp1/Battle.cs
+++ ConsoleApp1/Battle.cs
@@ -6,10 +6,31 @@ namespace ConsoleApp1
 {
     class Battle
     {
+
         // This is a utility class so it makes sense
         // to have just static methods
 
-       
+        // Recieve both Warrior objects
+        public static void StartFight(Warrior warrior1,
+            Warrior warrior2)
+        {
+            // Loop giving each Warrior a chance to attack
+            // and block each turn until 1 dies
+            while (true)
+            {
+                if (GetAttackResult(warrior1, warrior2) == "Game Over")
+                {
+                    Console.WriteLine("Game Over");
+                    break;
+                }
+
+                if (GetAttackResult(warrior2, warrior1) == "Game Over")
+                {
+                    Console.WriteLine("Game Over");
+                    break;
+                }
+            }
+        }
 
     }
 }
