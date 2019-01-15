e076d14ecac61a7a17254385c80c51b9df44792e
DavidLejeune <Lejeune.David@outlook.Com>
2019-01-15 13:50:06 +0100

    ICOmmand interface

---
+++ ConsoleApp1/ICommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    interface ICommand
+    {
+        // We can model what happens when
+        // a button is pressed for example
+        // a power button. By breaking
+        // everything down we can add
+        // an infinite amount of flexibility
+        void Execute();
+        void Undo();
+    }
+}
