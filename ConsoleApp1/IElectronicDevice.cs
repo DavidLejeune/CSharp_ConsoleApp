f51a5e59fc71c8520f3ff4295e86b1470931d2fa
DavidLejeune <Lejeune.David@outlook.Com>
2019-01-15 13:45:42 +0100

    interface Electronic Device

---
+++ ConsoleApp1/IElectronicDevice.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    interface IElectronicDevice
+    {
+        // We want each device to have 
+        // these capabilities
+        void On();
+        void Off();
+        void VolumeUp();
+        void VolumeDown();
+
+    }
+}
